468d83d8776dcd20cd807e038225c0a694f63a78
Herb Miller <herb@bobbingwide.com>
2019-10-30 15:00:29 +0000

    Issue #7 - Enqueue stylesheets using the recommended method

--- functions.php
+++ functions.php
@@ -1,4 +1,4 @@
-<?php // (C) Copyright Bobbing Wide 2013
+<?php // (C) Copyright Bobbing Wide 2013, 2019
 
 /**
  * Function to invoke when functions.php has been loaded
@@ -6,6 +6,7 @@
 function oobit_functions_loaded() {
   add_theme_support( 'woocommerce' );
   add_action( "oik_admin_menu", "oobit_admin_menu" );
+  add_action( 'wp_enqueue_scripts', 'oobit_enqueue_styles' );
 }
   
 /**
@@ -27,7 +28,18 @@ function oobit_remove_menus() {
   if( !current_user_can( 'add_users' ) ){
     remove_menu_page( 'jetpack' );
   }
-} 
+}
+
+
+function oobit_enqueue_styles() {
+	$parent_style = 'twenty-eleven-style';
+	wp_enqueue_style( $parent_style, get_template_directory_uri() . '/style.css' );
+	wp_enqueue_style( 'oobit-style',
+		get_stylesheet_directory_uri() . '/style.css',
+		array( $parent_style ),
+		wp_get_theme()->get('Version')
+	);
+}
  
 oobit_functions_loaded();
 
